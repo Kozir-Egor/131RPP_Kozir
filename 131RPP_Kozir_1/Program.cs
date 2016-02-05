ed5df822a5520b99fbee72a342b1c01b161898f0
Kozir_Egor <wok9@yandex.ru>
2016-02-05 11:07:44 +0300

    https://github.com/Kozir-Egor/131RPP_Kozir.git

--- 131RPP_Kozir_1/Program.cs
+++ 131RPP_Kozir_1/Program.cs
@@ -10,6 +10,15 @@ namespace _131RPP_Kozir_1
     {
         static void Main(string[] args)
         {
+            int x, y;
+            Console.WriteLine("Введите x");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите y");
+            y = Convert.ToInt32(Console.ReadLine());
+            int z;
+            z = x + y;
+            Console.WriteLine("{1}+{2}={0}", z, x, y);
+            Console.ReadKey();
         }
     }
 }
